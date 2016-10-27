8a42d0a21a7ce9d2e5939cd5255673938ec69f08
Joaquín Filipic <jfilipic@itba.edu.ar>
2016-10-27 01:59:40 +0000

    Test.java edited online with Bitbucket

--- flow/Test.java
+++ flow/Test.java
@@ -12,32 +12,66 @@ public class Test {
 	
 		int fils;
 		int cols;
-		int[][] matrix,FinalMatrixHC;
+		int[][] matrix;
+		Point[][] solution;
+		
+		fils = examples.get(0).fils;
+		cols = examples.get(0).cols;
+		matrix = examples.get(0).matrix;
+		
+		System.out.println("Entrada del hill climbing: " + "problema: 0");
+		Algorithm.printMatrix(fils, cols, matrix);
+		System.out.println("");
+		
+		long total_time = 100000;
+		solution = HillClimbing.solve(fils, cols, matrix, total_time);
+		System.out.println("Salida del hill climbing:");
+		Algorithm2.printMatrix(fils, cols, solution);
+		System.out.println("");
+		
+		/*
+		for (int i = 0; i < examples.size(); i++) {
+			fils = examples.get(i).fils;
+			cols = examples.get(i).cols;
+			matrix = examples.get(i).matrix;
+			
+			System.out.println("Entrada del algoritmo2:" + " problema: " + i);
+			//Algorithm.printMatrix(fils, cols, matrix);
+			System.out.println("");
+			
+			long start_time = System.currentTimeMillis();
+			solution = Algorithm2.solve(fils, cols, matrix);
+			long total_time = System.currentTimeMillis() - start_time;
+			
+			System.out.println("Salida del algoritmo2:");
+			//Algorithm2.printMatrix(fils, cols, solution);
+			System.out.println("");
+			
+			System.out.println("Tiempo total de ejecuci�n: " + total_time);
+			System.out.println("");
+		}
 		
 		for (int i = 0; i < examples.size(); i++) {
 			fils = examples.get(i).fils;
 			cols = examples.get(i).cols;
 			matrix = examples.get(i).matrix;
-
-			System.out.println("SOLUCION APROXIMADA:");
-			System.out.println("Entrada del algoritmo:" + " problema: " + i);
-			AlgorithmHC.printMatrix(fils, cols, matrix);
+			
+			System.out.println("Entrada del algoritmo1:" + " problema: " + i);
+			//Algorithm.printMatrix(fils, cols, matrix);
 			System.out.println("");
-			FinalMatrixHC = AlgorithmHC.solve(fils, cols, matrix);
-			System.out.println("Salida del algoritmo:");
-			AlgorithmHC.printMatrix(fils, cols, FinalMatrixHC);
-			System.out.println("");
-
-
-			System.out.println("SOLUCION EXACTA:");
-			System.out.println("Entrada del algoritmo:" + " problema: " + i);
-			Algorithm.printMatrix(fils, cols, matrix);
-			System.out.println("");
+			
+			long start_time = System.currentTimeMillis();
 			matrix = Algorithm.solve(fils, cols, matrix);
-			System.out.println("Salida del algoritmo:");
-			Algorithm.printMatrix(fils, cols, matrix);
+			long total_time = System.currentTimeMillis() - start_time;
+			
+			System.out.println("Salida del algoritmo1:");
+			//Algorithm.printMatrix(fils, cols, matrix);
+			System.out.println("");
+			
+			System.out.println("Tiempo total de ejecuci�n: " + total_time);
 			System.out.println("");
 		}
+		*/
 	}
 	
 }
